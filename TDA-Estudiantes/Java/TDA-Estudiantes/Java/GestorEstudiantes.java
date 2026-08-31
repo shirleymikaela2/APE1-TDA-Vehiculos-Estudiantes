9afc90a670e15a300f29a725cfa0f6b42b00bb1e
josue2004 <caguanalenin32@gmail.com>
2026-08-30 20:05:18 -0500

    feat: crear gestor de estudiantes en Java

--- TDA-Estudiantes/Java/TDA-Estudiantes/Java/GestorEstudiantes.java
+++ TDA-Estudiantes/Java/TDA-Estudiantes/Java/GestorEstudiantes.java
@@ -1,1 +1,45 @@
+public class GestorEstudiantes {
 
+    private Estudiante[] estudiantes;
+    private int cantidadEstudiantes;
+
+    public GestorEstudiantes() {
+
+        estudiantes = new Estudiante[20];
+        cantidadEstudiantes = 0;
+    }
+
+    public boolean agregarEstudiante(Estudiante estudiante) {
+
+        if (cantidadEstudiantes < 20) {
+
+            estudiantes[cantidadEstudiantes] = estudiante;
+            cantidadEstudiantes++;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public Estudiante buscar(String cedula) {
+
+        for (int i = 0; i < cantidadEstudiantes; i++) {
+
+            if (estudiantes[i].getCedula().equals(cedula)) {
+
+                return estudiantes[i];
+            }
+        }
+
+        return null;
+    }
+
+    public Estudiante[] getEstudiantes() {
+        return estudiantes;
+    }
+
+    public int getCantidadEstudiantes() {
+        return cantidadEstudiantes;
+    }
+}
